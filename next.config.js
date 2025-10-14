d7bc58fe0acfb72568e2a0bcdfff377d2f3de097
jonjamestudor-hash <jonjamestudor@gmail.com>
2025-10-14 16:07:20 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false, transpilePackages: [], output: 'standalone' };
+const nextConfig = {
+  reactStrictMode: false,
+  transpilePackages: [],
+  output: 'standalone',
+  // Tell Next.js to use your custom folder instead of /public
+  distDir: 'rem-webapp'
+};
 module.exports = nextConfig;
