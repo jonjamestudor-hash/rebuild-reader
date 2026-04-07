a79ee18adac7373b0b5949bc704bebe743aabf37
jonjamestudor-hash <jonjamestudor@gmail.com>
2026-04-07 11:25:23 -0700

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() { return null; }
-
-export async function getServerSideProps({ res }) {
-  // Redirect "/" to the static reader HTML
-  res.writeHead(302, { Location: '/index.html' });
-  res.end();
-  return { props: {} };
-}
